82876fe51045c203b92954a5a2e72227d94742e2
Anna Sedlackova <axs1202@case.edu>
2019-03-01 20:56:41 -0500

    fixed comments, added tests plus backend api calls from front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,42 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import './App.css';
+import $ from 'jquery'; 
 
+const axios = require('axios');
+
+/** Class that handles small popup when a district is clicked */
 class Popup extends React.Component {
   render() {
     return (
       <div className='popup'>
         <div className='popup_inner'>
           <p>{this.props.text}</p>
-        <button onClick={this.props.closePopup}>Back</button>
-        <button onClick={this.props.toggleFullView}>More Details</button>
+          <button onClick={this.props.closePopup}>Back</button>
+          <button onClick={this.props.toggleFullView}>More Details</button>
         </div>
       </div>
     );
   }
 }
 
+/** Class that handles small popup when a district is clicked */
 class FullView extends React.Component {
   render() {
     return (
       <div className='full_view'>i
         <div className='full_view_inner'>
           <p>{this.props.text}</p>
-        <button className = "square" onClick={this.props.closeFullView}>Back</button>
+
+          // goes back to the main page
+          <button className = "square" onClick={this.props.closeFullView}>Back</button>
         </div>
       </div>
     )
   }
 }
 
+/** Map implementation for the front page */
 class SimpleMap extends React.Component {
   static defaultProps = {
     center: {
@@ -40,6 +48,7 @@ class SimpleMap extends React.Component {
  
   render() {
     return (
+
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
@@ -53,22 +62,88 @@ class SimpleMap extends React.Component {
   }
 }
 
+// Main class
 class App extends Component {
+
+/**
+ * Manages the state of the popup components
+ * @constructor
+ */
   constructor(props) {
     super(props);
     this.state = {
       value: null,
       showPopup: false,
-      showFullView: false
+      showFullView: false,
+      map: null
     };
+    this.popupInfo = this.popupInfo.bind(this)
+    this.fullViewInfo = this.fullViewInfo.bind(this)
   }
 
+  /* Updates map on startup*/
+  componentWillMount() {
+    axios.get('localhost:3500/map').then(function(response) {
+      console.log(response);
+      /**
+      * get data from response
+      * this.setState({map: response})
+      **/
+    })
+    .catch (function (error){
+      console.log(error);
+    })
+    .then (function (){
+      //always executed
+    });
+  }
+
+  /* Gets info for the popup*/
+  popupInfo() {
+    axios.get('localhost:3500/summary').then(function(response) {
+      console.log(response);
+      /**
+      * get data from response
+      * this.setState({map: response})
+      **/
+    })
+    .catch (function (error){
+      console.log(error);
+    })
+    .then (function (){
+      //always executed
+    });
+  }
+
+  /* Gets info for the fullview*/
+  fullViewInfo() {
+    axios.get('localhost:3500/details').then(function(response) {
+      console.log(response);
+      /**
+      * get data from response
+      * this.setState({map: response})
+      **/
+    })
+    .catch (function (error){
+      console.log(error);
+    })
+    .then (function (){
+      //always executed
+    });
+  }
+
+  /**
+  * Changes the state of the popup
+  */
   togglePopup() {
     this.setState({
       showPopup: !this.state.showPopup
     });
   }
 
+  /**
+  * Changes the state of the full view
+  */
   toggleFullView() {
     this.setState({
       showPopup: !this.state.showPopup,
@@ -76,16 +151,15 @@ class App extends Component {
     });
   }
 
+  /**
+  * Fully renders the application
+  */
   render() {
     const status = 'voda';
     return (
       <div className = "App">
-
-        <div className ="header"> 
-        {status}
-        </div>
+        <div className ="header"> {status} </div>
         <button className = "square" onClick={this.togglePopup.bind(this)}>Show</button>
-  
         {this.state.showPopup ? 
           <Popup
             text='Contaminents Detail'
@@ -93,16 +167,14 @@ class App extends Component {
             toggleFullView ={this.toggleFullView.bind(this)} />
           : null
         }
-
         {this.state.showFullView ? 
           <FullView
             text='VODA'
             closeFullView ={this.toggleFullView.bind(this)} />
           : null
         }
-
       </div>
-      );
+    );
   }
 }
 
