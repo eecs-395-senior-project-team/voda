adf6af67eb226e04cc1860ae08977012f6dc15a3
DavidANixon <Dman42997@gmail.com>
2019-03-06 17:23:28 -0500

    fixed model problems

--- backend/vodabackend/vodaMainApp/models.py
+++ backend/vodabackend/vodaMainApp/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class State(models.Model):
+    state_id = models.CharField(max_length=2, primary_key=True)
+
 class Sources(models.Model):
     source_id = models.AutoField(primary_key=True)
     utility_name = models.CharField(max_length=200)
@@ -12,9 +15,9 @@ class Sources(models.Model):
 class Contaminants(models.Model):
     contaminant_id = models.AutoField(primary_key=True)
     contaminant_name = models.CharField(max_length=200)
-    health_guideline = models.CharField(max_length=200)
-    legal_limit = models.CharField(max_length=200)
-    national_avg = models.DecimalField()
+    health_guideline = models.DecimalField(decimal_places=3, max_digits=10)
+    legal_limit = models.DecimalField(decimal_places=3, max_digits=10)
+    national_avg = models.DecimalField(decimal_places=3, max_digits=10)
     summary = models.TextField()
     health_concerns = models.TextField()
     long_health_concerns = models.TextField()
@@ -22,12 +25,9 @@ class Contaminants(models.Model):
 class SourceLevels(models.Model):
     source_id = models.ForeignKey(Sources, on_delete=models.CASCADE)
     contaminant_id = models.ForeignKey(Contaminants, on_delete=models.CASCADE)
-    contaminant_level = models.CharField(max_length=200)
-
-class State(models.Model):
-    state_id = models.CharField(max_length(2), primary_key=True)
+    contaminant_level = models.DecimalField(decimal_places=3, max_digits=10)
 
 class State_Avg_Levels(models.Model):
     state_id = models.ForeignKey(State, on_delete=models.CASCADE)
     contaminant_id = models.ForeignKey(Contaminants, on_delete=models.CASCADE)
-    state_avg = models.DecimalField()
+    state_avg = models.DecimalField(decimal_places=3, max_digits=10)
