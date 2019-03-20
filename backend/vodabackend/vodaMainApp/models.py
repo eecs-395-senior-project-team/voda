d847fa4e474950f3dcf76db18580ae56f82dadee
David Fan <dfan14051@gmail.com>
2019-03-20 12:11:29 -0400

    Add score field to model

--- backend/vodabackend/vodaMainApp/models.py
+++ backend/vodabackend/vodaMainApp/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 class State(models.Model):
     state_id = models.CharField(max_length=2, primary_key=True)
+
 
 class Sources(models.Model):
     source_id = models.AutoField(primary_key=True)
@@ -10,7 +12,9 @@ class Sources(models.Model):
     city = models.CharField(max_length=200)
     state = models.ForeignKey(State, on_delete=models.CASCADE)
     number_served = models.IntegerField()
+    score = models.FloatField()
     # TODO: Add congressional information etc.
+
 
 class Contaminants(models.Model):
     contaminant_id = models.AutoField(primary_key=True)
@@ -22,10 +26,12 @@ class Contaminants(models.Model):
     health_concerns = models.TextField()
     long_health_concerns = models.TextField()
 
+
 class SourceLevels(models.Model):
     source_id = models.ForeignKey(Sources, on_delete=models.CASCADE)
     contaminant_id = models.ForeignKey(Contaminants, on_delete=models.CASCADE)
     contaminant_level = models.DecimalField(decimal_places=3, max_digits=10)
+
 
 class State_Avg_Levels(models.Model):
     state_id = models.ForeignKey(State, on_delete=models.CASCADE)
