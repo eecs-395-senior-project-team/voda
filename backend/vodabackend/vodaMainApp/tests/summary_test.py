c03ae75e152ba9927523eec0b77fd57a36154f18
David Navia <dan57@case.edu>
2019-03-21 12:09:40 -0400

    Updated the tests for the backend NOTE one of the backend tests still doesn't work but it will be updated soon

--- backend/vodabackend/vodaMainApp/tests/summary_test.py
+++ backend/vodabackend/vodaMainApp/tests/summary_test.py
@@ -1,47 +1,37 @@
 """
 Tests for summary endpoint.
 """
-import requests
+from vodabackend.vodaMainApp.views import summary
 
 
-###############################################################################
-# Helper functions.
-###############################################################################
-def request_summary_with_source():
-    """
-    call for the summary endpoint with a source value used by the source tests
-    """
-    # Send a request to the API server and store the response.
-    return requests.get('http://0.0.0.0:8000/summary?source=7')
-
-
-def request_summary_no_source():
-    """
-    call for the summary with no source value endpoint used by the source tests
-    """
-    # Send a request to the API server and store the response.
-    return requests.get('http://0.0.0.0:8000/summary')
-
+requestWithSource = '/summary?source=7'
+requestNoSource = '/summary'
 
 ###############################################################################
 # Tests.
 ###############################################################################
-def test_summary_status_code_with_soure():
+def test_summary_status_code_with_soure(rf):
     """
     test that the status_code of the summary http response is 200 when it has a source value
     """
-    assert request_summary_with_source().status_code == 200
+    request = rf.get(requestWithSource)
+    response = summary(request)
+    assert response.status_code == 200
 
 
-def test_summary_content():
+def test_summary_content(rf):
     """
     test that the summary http response body returns the proper message when it has a source value
     """
-    assert request_summary_with_source().content == b'Returns the summary details for water supply 7.'
+    request = rf.get(requestWithSource)
+    response = summary(request)
+    assert response.content == b'Returns the summary details for water supply 7.'
 
 
-def test_summary_status_code_no_soure():
+def test_summary_status_code_no_soure(rf):
     """
     test that the status_code of the summary http response is 400 if there's no source value
     """
-    assert request_summary_no_source().status_code == 400
+    request = rf.get(requestNoSource)
+    response = summary(request)
+    assert response.status_code == 400
