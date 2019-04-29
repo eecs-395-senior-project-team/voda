fe3d648362aaa9448f88413eb00b06f6eabf1311
Anna Sedlackova <axs1202@case.edu>
2019-04-28 20:03:58 -0400

    making progress

--- frontend/src/DetailView.js
+++ frontend/src/DetailView.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Axios from 'axios';
 import Log from './Log';
-import './DetailView.sass'
+import './DetailView.sass';
+import Tabs from 'react-bootstrap/Tabs';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Tab from 'react-bootstrap/Tab';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 /**
  * DetailView Component.
@@ -43,7 +48,95 @@ class DetailView extends Component {
     const { details } = this.props;
     const { hideDetailView, countyName } = this.props;
     return (
-      <div className="Details border border-dark rounded">Place Detail View Here</div>
+      <div className="Details border border-dark rounded">
+        <h1>Contaminant Details for {countyName} County</h1>
+        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+          <Tab eventKey="home" title="Home">
+
+            <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
+              <Row>
+                <Col sm={4}>
+                  <ListGroup>
+                    <ListGroup.Item action href="#link1">
+                      Horrible Contaminent 1
+                    </ListGroup.Item>
+                    <ListGroup.Item action href="#link2">
+                      Horrible Contaminent 2
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Col>
+                <Col sm={8}>
+                  <Tab.Content>
+                    <Tab.Pane eventKey="#link1">
+                      <p>HI</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="#link2">
+                      <p>HI</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Col>
+              </Row>
+            </Tab.Container>
+
+          </Tab>
+          <Tab eventKey="profile" title="Profile">
+
+            <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
+              <Row>
+                <Col sm={4}>
+                  <ListGroup>
+                    <ListGroup.Item action href="#link1">
+                      Bad Contaminent 1
+                    </ListGroup.Item>
+                    <ListGroup.Item action href="#link2">
+                      Bad Contaminent 2
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Col>
+                <Col sm={8}>
+                  <Tab.Content>
+                    <Tab.Pane eventKey="#link1">
+                      <p>HI</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="#link2">
+                      <p>HI</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Col>
+              </Row>
+            </Tab.Container>
+
+          </Tab>
+          <Tab eventKey="contact" title="Contact">
+
+            <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
+              <Row>
+                <Col sm={4}>
+                  <ListGroup>
+                    <ListGroup.Item action href="#link1">
+                      Good Contaminent 1
+                    </ListGroup.Item>
+                    <ListGroup.Item action href="#link2">
+                      Good Contaminent 2
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Col>
+                <Col sm={8}>
+                  <Tab.Content>
+                    <Tab.Pane eventKey="#link1">
+                      <p>HI</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="#link2">
+                      <p>HI</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Col>
+              </Row>
+            </Tab.Container>
+
+          </Tab>
+        </Tabs>
+     </div>
     );
   }
 }
